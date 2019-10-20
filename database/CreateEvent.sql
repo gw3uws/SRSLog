616dedbd8486189a597e087ca56d9af2ed2a66b4
Peter Barnes <pe5erb@gmail.com>
2019-10-21 00:41:42 +0100

    Update CreateEvent.sql

--- database/CreateEvent.sql
+++ database/CreateEvent.sql
@@ -1,6 +1,6 @@
-CREATE TABLE `m0swn097_srslog`.`yotamonth17`
+CREATE TABLE `srslog_db`.`eventname`
  ( `LogID` INT NOT NULL AUTO_INCREMENT ,
   `Call` TEXT NOT NULL , `QSO_Date` TEXT NOT NULL ,
    `Time_On` TEXT NOT NULL , `Band` TEXT NOT NULL ,
-    `Mode` TEXT NOT NULL , PRIMARY KEY (`LogID`)) 
-    ENGINE = InnoDB;
+    `Mode` TEXT NOT NULL , PRIMARY KEY (`LogID`)
+ );
