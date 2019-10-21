22dc3270c5b9ad640428caa575167bbb2e706e1c
Your Name <pe5erb@gmail.com>
2019-10-21 00:46:55 +0000

    comments

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//Include the standard db connect script
 include 'database/connect.php';
 
 //Fetch event details from database
@@ -10,52 +11,53 @@ if ($result=mysqli_fetch_assoc(mysqli_query($con,"SELECT * FROM event WHERE 1"))
   $Event_url  = $result['URL'];
 
 //Setup Frontend event title, description and logo
-echo '<html><head><title>SRS Log</title><link rel="stylesheet" type="text/css" href="stylesheet.css?v=' . time() . '">';
+echo '<html><head><title>' . $Event_name . ' Log</title><link rel="stylesheet" type="text/css" href="stylesheet.css?v=' . time() . '">';
 echo '<script language="javascript" type="text/javascript" src="jquery.js"></script>';
 echo '<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"></head><body>';
 echo '<div class="loginlink" id="loginlink"><a href="users/login.php">Login</a></div>';
-echo '<h1>' . $Event_name . '</h1><p>' . $Event_desc . '</p>';
+echo '<h1>' . $Event_name . '</h1><h3>' . $Event_desc . '</h3>';
 echo "<div class='logo'><center><a href='".$Event_url."' target='_blank'><img src='".$Event_img."' alt='YOTA Logo' height='10%' align='middle'></a></center></div>";
 echo "<br><br>";
 echo '<div class="qso_count" id="qso_count">Total Contacts<br>0</div><br><br>';
 echo '<div class="lastrow" id="lastrow"></div>';
-echo '<div class="advert" id="advert">Experience amateur radio in B001</div>';
 
+//Javascript that pings the database to get the number of contacts. TODO: merge these two scripts
 echo "<script type = 'text/javascript'>
 setInterval(function(){
 function update() {
   $(function () {
-    $.ajax({                                      
-      url: 'database/getrows.php',  
-      data: '',          	                          
-      dataType: 'json',                	
+    $.ajax({
+      url: 'database/getrows.php',
+      data: '',
+      dataType: 'json',
       success: function(data){
         var qso_count = data;
     	document.getElementById('qso_count').innerHTML = 'Total Contacts' + '<br>' + qso_count;
-      } 
+      }
     });
-  }); 
+  });
  }
 update();
-},1000);</script>";
+},1000);</script>"; //Updates every 1000ms
 
+//Javascript that pulls the last contact out of the db
 echo "<script type = 'text/javascript'>
 setInterval(function(){
 function update2() {
   $(function () {
-    $.ajax({                                      
-      url: 'database/getlastrow.php',  
-      data: '',          	                          
-      dataType: 'json',                
+    $.ajax({
+      url: 'database/getlastrow.php',
+      data: '',
+      dataType: 'json',
       success: function(data){
         var row = data;
     	document.getElementById('lastrow').innerHTML = row[0] + ' on ' + row[2];
-      } 
+      }
     });
-  }); 
+  });
  }
 update2();
-},1000);</script>";
+},1000);</script>"; //Updates every 1000ms
 
 mysqli_close($con);
 
